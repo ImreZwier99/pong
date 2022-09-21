0a25c0f8c0e117f141f0c0f3876cb5a21f54dcf3
ImreZwier99 <ifrzwiers@st.noorderpoort.nl>
2022-09-21 11:56:08 +0100

    edrh

---
+++ pong2/Assets/PlayerMovement.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 10;
+    public int playerNumber = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerNumber == 2)
+        {
+            transform.Translate(new Vector3(0, Input.GetAxis("Player2") * speed * Time.deltaTime));
+        }
+            
+        if (playerNumber == 1)
+        {
+            transform.Translate(new Vector3(0, Input.GetAxis("Player1") * speed * Time.deltaTime));
+        }
+    }
+
+}
